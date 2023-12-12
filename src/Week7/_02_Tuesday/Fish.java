4d96de3ba5dddc22799ed5fe12de7a36ba7d36b9
aslanbekturganbayev <aslanbeksmailbox@gmail.com>
2023-12-12 12:50:47 -0500

    new commit

---
+++ src/Week7/_02_Tuesday/Fish.java
@@ -0,0 +1,28 @@
+package Week7._02_Tuesday;
+
+public class Fish extends Animal{
+    private boolean isOcean;
+
+    public Fish(String type, int age, boolean isOcean) {
+        super(type, age);
+        this.isOcean = isOcean;
+    }
+
+    public boolean isOcean() {
+        return isOcean;
+    }
+
+    public void setOcean(boolean ocean) {
+        isOcean = ocean;
+    }
+
+    @Override
+    public void eating() {
+        System.out.println("Fish is eating");
+    }
+    public void swimming(){
+        if (this.isOcean()){
+            System.out.println("");
+        }
+    }
+}
