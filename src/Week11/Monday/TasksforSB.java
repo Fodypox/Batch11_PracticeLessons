7714a992815645a6dbaa42ee5a3031a6a341cdd5
aslanbekturganbayev <aslanbeksmailbox@gmail.com>
2024-01-08 12:50:32 -0500

    new commit

--- src/Week11/Monday/TasksforSB.java
+++ src/Week11/Monday/TasksforSB.java
@@ -2,6 +2,16 @@ package Week11.Monday;
 
 public class TasksforSB {
     public static void main(String[] args) {
-        /**/
+        /**
+         * we have String Hello world, and we need to add beautiful between them;
+         * */
+        String original = "Hello world";
+        String toInsert = " beautiful";
+
+        StringBuilder sb = new StringBuilder(original);
+
+        int index = 5;
+        sb.insert(index,toInsert);
+        System.out.println(sb.toString());
     }
 }
